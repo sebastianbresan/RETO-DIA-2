f99966ce5814fde1eb935b33045279dd361333a2
sebastianbresan <91628231+sebastianbresan@users.noreply.github.com>
2021-11-09 13:30:02 +0100

    Solucionado error en interes

--- js/js.js
+++ js/js.js
@@ -9,18 +9,20 @@ let cuotas = parseInt(slider);
 let tipo = null;
 let interes;
 if(document.getElementById('variable').checked){
-    interes = 1.5;
+    interes = "1,5";
+    int = 1.015;
     tipo= "variable"
 }
 else{
-    interes = 2.2;
+    interes = "1,22";
+    int = 1.022;
     tipo = "anual"
 }
 
-let resultado = (monto*interes)/cuotas;
+let resultado = ((monto/cuotas)*int);
 
 document.getElementById(`resultado`).innerHTML = resultado;
 
-alert("Usted ha seleccionado un monto de " + monto + "€ en "+ cuotas + " cuotas, con una tasa " + tipo + " del " + interes + ". Su cuota mensual es de " + resultado + "€" );
+alert("Usted ha seleccionado un monto de " + monto + "€ en "+ cuotas + " cuotas, con una tasa " + tipo + " del " + interes + "%. Su cuota mensual es de " + Math.round(resultado)+ "€" );
     
 }
